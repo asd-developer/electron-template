0b50cca5a70a2b4db6dbc74b86abb5f5e48758d2
André Duarte <andre.duarte@frotcom.com>
2021-11-28 01:27:12 +0000

    first commit

---
+++ src/App.ts
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default Vue.extend({
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+});
---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+      <h1>Hellooooo</h1>
+  </div>
+</template>
+
+<script lang="ts" src="./App.ts"/>
+<style lang="scss" src="./App.scss"/>
